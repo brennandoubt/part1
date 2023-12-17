182d15a34a54c29f262ef21a75f70d2ac3c1b978
brennandoubt <brennandoubt@gmail.com>
2023-12-16 20:33:32 -0600

    completed unicafe step1

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
+  return (
+    <div>
+      <Header text='give feedback' />
+
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      
+      <Header text='statistics' />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
