1e0fcfda622aa65835a726e7f0803e82d8426f07
brennandoubt <brennandoubt@gmail.com>
2023-12-16 21:33:20 -0600

    unicafe step6 (with json parse warning in console)

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,7 +5,14 @@ const Header = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({good, bad, neutral}) => {
   if (good + neutral + bad === 0) {
@@ -17,14 +24,16 @@ const Statistics = ({good, bad, neutral}) => {
   const positive = (good/all) * 100
 
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive} />
-    </div>
+    <table>
+      <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={all} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
