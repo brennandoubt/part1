7d7fa2e5216522367383f6dfd99b2993376b662a
brennandoubt <brennandoubt@gmail.com>
2023-12-16 20:08:35 -0600

    Part 1d

--- learning/src/App.jsx
+++ learning/src/App.jsx
@@ -1,69 +1,95 @@
 /**
- * 1c) Component state, event handlers
+ * 1d) A more complex state, debugging React apps
  */
 
-import {useState} from 'react'
+import { useState } from "react"
 
-// "onSomething" = event handler props, "handleSomething" = event handler function
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const Display = ({counter}) => <div>{counter}</div>
+
+
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  //const [left, setLeft] = useState(0)
+  //const [right, setRight] = useState(0)
+  /*
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  // event handler can reference a function or define one explicitly
-  // handler can be a function or function reference (function call causes instant re-renders from setCounter)
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+    // const newClicks = {
+    //   left: clicks.left + 1,
+    //   right: clicks.right
+    // }
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1
+    }  // ...clicks creates a new object copying all properties of clicks object with updated specified property like "left" here
+    setClicks(newClicks)
+  }
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }  // ALWAYS change state by setting it to a new object
+  */
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L')) // concat returns new copy of array with 'L' added
+    const updatedLeft = left + 1  // to fix asynchronous state update in React, so update the state in a separate variable first
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
+  // never mutate state directly, can cause problems that are hard to debug
+  // never define components inside other components
+
+  const hello = (who) => () => {
+    console.log('hello', who)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
 
 export default App
-
-/*
-// Stateful components
-import {useState} from 'react'
-
-const App = () => {
-  // adds "state" to component
-  const [counter, setCounter] = useState(0) // stateful value (0) + its update function
-
-  // 1s timeout where function passed is called
-  setTimeout(
-    //  1. setCounter call re-renders 'App' component to modify counter state,
-    //  also re-executes code in component function body
-    //  2. useState called to return modified state value 1
-    //  3. setTimeout called to execute 1s timeout
-    //  4. setCounter called at timeout to increment counter state again
-    //  ^repeats until app stops running
-     
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
-
-  return (
-    <div>{counter}</div>
-  )
-}
-export default App
-*/
