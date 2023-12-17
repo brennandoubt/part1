5c2844610556b062d03232857093ebe983fb663e
brennandoubt <brennandoubt@gmail.com>
2023-12-16 20:52:40 -0600

    unicafe step2

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -36,6 +36,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {((good * 1) + (neutral * 0) + (bad * -1))/(good + neutral + bad)}</p>
+      <p>positive {(good/(good + neutral + bad)) * 100} %</p>
     </div>
   )
 }
